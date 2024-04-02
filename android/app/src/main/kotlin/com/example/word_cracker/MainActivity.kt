ae881c683d901528119a5f67caabf3e238f9e90f
Thomas <thomas.arnoux98@gmail.com>
2024-04-03 00:03:05 +0800

    app icons + set up

--- android/app/src/main/kotlin/com/example/word_cracker/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/word_cracker/MainActivity.kt
@@ -1,5 +1,6 @@
-package com.example.word_cracker
+package fr.thomas_arnoux.word_cracker
 
 import io.flutter.embedding.android.FlutterActivity
 
-class MainActivity: FlutterActivity()
+class MainActivity: FlutterActivity() {
+}
